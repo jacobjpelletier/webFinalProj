f06de776d5d7e0a1c3b93dd52520084d7fc8c50e
jacobjpelletier <jacobjpelletier@gmail.com>
2021-11-29 13:51:32 -0500

    added user jacob PW: wildberry to blog to test features

--- base/models.py
+++ base/models.py
@@ -2,11 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+
 class Topic(models.Model):
     name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
+
 
 class Room(models.Model):
     host = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
@@ -22,6 +24,7 @@ class Room(models.Model):
 
     def __str__(self):
         return self.name
+
 
 class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
